754778ade12bd9c68dac72e2330facb77fb6cb5f
dldbs <dldbs@DESKTOP-RRO0K5L>
2024-03-28 13:09:05 +0900

    tlqkf

---
+++ javaPrograming/src/week4/ArithmeticOperator.java
@@ -0,0 +1,29 @@
+package week4;
+
+import java.util.Scanner;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println("정수를 입력하시오:");
+		int time = scanner.nextInt();
+		int second = time % 60;
+		int minute = (time/60) % 60;		
+		int hour = (time/60) / 60;
+		
+		System.out.println(time + "초는");     
+		System.out.println(hour + "시간,");
+		System.out.println(minute + "분");
+		System.out.println(second + "초입니다.");
+		
+		scanner.close();
+		
+		
+	}
+
+}
+
+
---
+++ javaPrograming/src/week4/week5.java
@@ -0,0 +1,17 @@
+package week4;
+
+public class week5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		i=i*3;
+		
+		int j = i;
+		
+		
+		System.out.println(" i= " + i);
+		System.out.println(" j= " + j);
+	}
+
+}
