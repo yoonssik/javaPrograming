efd344e0d3dc54ea667bef58f9d727c25a8e3d6c
dldbs <dldbs@DESKTOP-RRO0K5L>
2024-05-21 23:31:52 +0900

    dd

---
+++ javaPrograming/src/week9/apapq.java
@@ -0,0 +1,15 @@
+package week9;
+
+public class apapq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0, i=1;
+		while(true) {
+			if(i>50) break;
+			sum += i;
+			i++;
+		}
+	}
+
+}
